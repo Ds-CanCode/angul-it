fb7b4517966a5eff95f0d16ec5f38554d04a620c
mohammed fadil <fadil.ue2006@gmail.com>
2026-01-27 17:02:46 +0100

    Fix Home page

--- angul_it/src/app/app.routes.ts
+++ angul_it/src/app/app.routes.ts
@@ -2,4 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./home-component/home-component').then(m => m.HomeComponent) },
+    // { path: '/captcha', loadComponent: () => import('./captcha-component/captcha-component').then(m => m.CaptchaComponent) },
 ];
--- angul_it/src/app/home-component/home-component.ts
+++ angul_it/src/app/home-component/home-component.ts
@@ -9,21 +9,13 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
   constructor(
-    //private router: Router,
+    private router: Router,
     // private captchaStateService: CaptchaStateService
   ) {}
 
-  // startChallenge(): void {
-  //   this.captchaStateService.resetCaptcha();
-  //   this.router.navigate(['/captcha']);
-  // }
+  startChallenge(): void {
+    // this.captchaStateService.resetCaptcha();
+    this.router.navigate(['/captcha']);
+  }
 
-  // continueChallenge(): void {
-  //   this.router.navigate(['/captcha']);
-  // }
-
-  // hasSavedProgress(): boolean {
-  //   const state = this.captchaStateService.getState();
-  //   return state.currentChallengeIndex > 0 || state.challenges.some(c => c.completed);
-  // }
 }
